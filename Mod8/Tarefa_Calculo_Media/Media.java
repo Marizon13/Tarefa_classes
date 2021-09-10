e524592fd6c3739bda6c686385d1a2e17efa10a0
unknown <chuck_teles@hotmail.com>
2021-09-10 16:55:53 -0300

    Tarefas

---
+++ Mod8/Tarefa_Calculo_Media/Media.java
@@ -0,0 +1,21 @@
+package NotasEscola;
+
+public class Media {
+	public static void main(String[] args) {
+		
+		// Notas do aluno Pedro
+		
+		System.out.println("Pedro Henrique ");
+		
+		
+		int nota1 = 9;
+		int nota2 = 4;
+		int nota3 = 7;
+		int nota4 = 10;
+		
+		int media =(nota1 + nota2 + nota3 + nota4)/4;
+		
+		System.out.println("nota media " + media);
+	}
+
+}
