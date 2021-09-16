3c228207057a833162a7bfd6b3cfc78baec08e78
unknown <chuck_teles@hotmail.com>
2021-09-16 08:26:21 -0300

    correção

---
+++ Modulo9/Tarefa_Wrappers/NovoTrab.java
@@ -0,0 +1,68 @@
+
+import java.util.Scanner;
+
+
+public class NovoTrab {
+
+    public static void main(String[] args) {
+		
+		// soma
+		Scanner z = new Scanner(System.in);
+		Scanner s = new Scanner (System.in);
+		
+
+
+		
+		System.out.println(" Digite dois valores para somar :");
+		
+		int num1 = z.nextInt();
+		int num2 = s.nextInt();
+		int num3 = num1 + num2;
+		
+		System.out.println(" A soma é : " + num3) ;
+		
+		// multiplicação
+
+		Scanner x = new Scanner(System.in);
+		Scanner y = new Scanner (System.in);
+		
+		System.out.println(" Digite dois valores para multiplicar :");
+		
+		Integer nu1 = z.nextInt();
+		Integer nu2 = s.nextInt();
+		Integer nu3 = nu1 * nu2;
+		
+		System.out.println(" resultado da multiplicado é : " + nu3) ;
+		
+		// Divisão
+		
+		Scanner a = new Scanner(System.in);
+		Scanner b = new Scanner (System.in);
+		
+		System.out.println(" Digite dois valores para dividir :");
+		
+		Float n1 = a.nextFloat();
+		Float n2 = b.nextFloat();
+		Float n3 = n1 / n2;
+		
+		System.out.println(" o resultado da divisão : " + n3); 
+		
+		
+		
+			
+		
+		}
+		
+
+		
+		
+	
+
+
+
+        
+    }
+
+
+
+    
