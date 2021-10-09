ec38d43ad7403e9dadafbf11d1695e2c1cf19a84
unknown <chuck_teles@hotmail.com>
2021-10-09 14:02:58 -0300

    Trabalho modulo 12

---
+++ Mod12/Tarefa_Coleções/Modulo12.java
@@ -0,0 +1,50 @@
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.Scanner;
+
+public class Modulo12 {
+	
+	
+	// 1 PARTE DO EXERCICIO
+
+	public static void main(String args[]) {
+
+		Scanner s = new Scanner(System.in);
+		System.out.println("Digite o nome separado por , ");
+		String nome = s.next();
+
+		String[] separador = nome.split(",");
+		Arrays.sort(separador);
+
+		for (String i : separador) {
+
+			System.out.println(i);
+
+		}
+
+		// 2 PARTE DO EXERCICIO
+
+		Scanner scanner = new Scanner(System.in);
+		ArrayList<String> feminino = new ArrayList();
+		ArrayList<String> masculino = new ArrayList();
+
+		System.out.println("Digite o Nome e o sexo separados por ,");
+		String nome1 = scanner.nextLine();
+
+		String[] separadores = nome1.split(",");
+		Arrays.sort(separadores);
+
+		for (int i = 0; i < separadores.length; i++) {
+			if (separadores[i].contains("-F")) {
+				feminino.add(separadores[i]);
+			} else {
+				masculino.add(separadores[i]);
+			}
+
+		}
+
+		System.out.println("Usuarios do Grupo Feminino" + feminino);
+		System.out.println("Usuarios do Grupo Masculino" + masculino);
+
+	}
+}
